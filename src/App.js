ba6603428c1b3251a5f1935a42abe56887debdb9
Eric <equality509@gmail.com>
2022-12-23 10:27:05 -0500

    movie app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react"
 import './App.css';
+import MovieDisplay from './components/MovieDisplay';
+import Form from './components/Form';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const apiKey = "d7009a9f"
+
+// declare our state
+const [movie, setMovie] = useState(null)
+
+// function to get movie
+const getMovie = async (searchTerm) => {
+  // Make api call and get back response
+  const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`)
+  // convert the response into a javascript object
+  const data = await response.json()
+  // update the movie state
+  setMovie(data)
+}
+
+console.log("log in the body of the component")
+  useEffect(()=>{
+    getMovie('Swiss Army Man')
+    console.log("log in the use effect")
+  }, [])
+
+return (
+  <div className="App">
+    <Form moviesearch={getMovie} />
+    <MovieDisplay movie={movie}/>
+  </div>
+);
 }
 
 export default App;
+
+
+
+
+
+
